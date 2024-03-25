650293f9bff36bc32c84d17686bce5d9ad9e1881
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2024-03-25 22:47:39 +0100

    API
    Pointage api map bings sur orleans

--- map.js
+++ map.js
@@ -1,22 +1,22 @@
-	
-	function initMap() {
+function initMap() {
     var map = new Microsoft.Maps.Map(document.getElementById('map'), {
         credentials: 'ArisYpVHG-7kW0JE9JPaSf8LJ0A6GEUmudzG4J4QWDcd_s0AtQ5n1ORO91H_oCyM' // Utilisez ici votre clé API Bing Maps
     });
 
-   // Coordonnées de Orléans, Loiret
-        var orleans = { lat: 47.902964, lng: 1.90925 };
-        
-        // Création de la carte centrée sur Orléans
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10, // Niveau de zoom
-            center: orleans // Centre de la carte
-        });
+    // Coordonnées de Orléans, Loiret
+    var orleans = new Microsoft.Maps.Location(47.902964, 1.90925);
 
-        // Marqueur pour Orléans
-        var marker = new google.maps.Marker({
-            position: orleans,
-            map: map,
-            title: 'Orléans, Loiret'
-        });
+    // Centrer la carte sur Orléans
+    map.setView({
+        center: orleans,
+        zoom: 12
+    });
+
+    // Création du marqueur pour Orléans
+    var marker = new Microsoft.Maps.Pushpin(orleans, {
+        title: 'Orléans, Loiret'
+    });
+
+    // Ajout du marqueur à la carte
+    map.entities.push(marker);
 }
